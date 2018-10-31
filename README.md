dfe2d4d0dafb54c968628751ef702039d7d4bf92
Linda de Haan <linda.dehaan@hotmail.com>
2018-10-31 16:36:06 +0100

    Added readme and the code of Folkert & Dennis

---
+++ README.md
@@ -0,0 +1,24 @@
+# Functional Programming
+
+## Process
+
+#### Day 1 | October 29, 2018
+* The kick-off of the course functional programming.;
+* Introduction assigment from someone from the OBA;
+* Connect with the API of the OBA;
+* Workshop Titus.
+
+#### Day 2 | October 30, 2018
+* No location to sit with everybody;
+* Sat in Theo Thijssen Huis;
+* Came up with five research questions.
+
+__*Research questions*__
+1. Which genre has become more populair after the year 2000?
+2. Has the amount of e-books decreased or increased throughout the years?
+3. How many Pixar items are there and how many Disney items?
+4. Has the amount of books with black & white illustrations decreased or increased throughout the years?
+5. __What is the difference in the amount of films & books Disney, Pixar & Dreamworks brought out starting from the year 2000?__
+
+#### Day 3 | October 31, 2018
+* 
--- index.js
+++ index.js
@@ -1,23 +1,66 @@
-const OBA = require('oba-api');
+require('dotenv').config()
 
-// Setup authentication to api server
-const client = new OBA({
-  // ProQuest API Keys
-  public: '1e19898c87464e239192c8bfe422f280',
-  secret: '4289fec4e962a33118340c888699438d'
-});
+const api = require('./oba-api.js')
+const chalk = require('chalk');
+const express = require('express')
+const app = express()
+const port = 3000
 
-// General usage:
-// client.get({ENDPOINT}, {PARAMS});
-// ENDPOINT = search | details | refine | schema | availability | holdings
-// PARAMS = API url parameter options (see api docs for more info)
-
-// Client returns a promise which resolves the APIs output in JSON
-
-// Example search to the word 'rijk' sorted by title:
-client.get('search', {
-  q: 'Stefanna',
-  sort: 'title'
+const obaApi = new api({
+  url: 'https://zoeken.oba.nl/api/v1/',
+  key: process.env.PUBLIC
 })
-  .then(res => console.log(JSON.parse(res))) // JSON results
-  .catch(err => console.log(err)) // Something went wrong in the request to the API
+
+// Search for method, params and than optional where you wanna find something
+// returns first 20 items
+// obaApi.get(endpoint, params, filterKey)
+// possible endpoints: search (needs 'q' parameter) | details (needs a 'frabl' parameter) | availability (needs a 'frabl' parameter) | holdings/root | index/x (where x = facet type (like 'book' ))
+// possible parameters: q, librarian, refine, sort etc. check oba api documentation for all
+// possible filterKey: any higher order key in response object, like title returns only title objects instead of full data object
+obaApi.get('search', {
+  q: 'dreamworks',
+  librarian: false,
+  refine: true,
+  facet: 'language(dut)'
+}, 'title').then(response => {
+
+  // response ends up here
+  console.log(response)
+
+  // Make server with the response on the port
+  app.get('/', (req, res) => res.json(response))
+  app.listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
+})
+
+
+
+
+
+//////////////////////////////////////////////////////////////
+// const OBA = require('oba-api');
+//
+// if(process.env.NODE_ENV !== 'production') {
+//   require('dotenv').load()
+// }
+//
+// // Setup authentication to api server
+// const client = new OBA({
+//   // ProQuest API Keys
+//   public: '1e19898c87464e239192c8bfe422f280',
+//   secret: '4289fec4e962a33118340c888699438d'
+// });
+//
+// // General usage:
+// // client.get({ENDPOINT}, {PARAMS});
+// // ENDPOINT = search | details | refine | schema | availability | holdings
+// // PARAMS = API url parameter options (see api docs for more info)
+//
+// // Client returns a promise which resolves the APIs output in JSON
+//
+// // Example search to the word 'rijk' sorted by title:
+// client.get('search', {
+//   q: 'dreamworks',
+//   sort: 'year'
+// })
+//   .then(res => console.log(JSON.parse(res))) // JSON results
+//   .catch(err => console.log(err)) // Something went wrong in the request to the API
